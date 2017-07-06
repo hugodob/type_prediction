d4b2f8d299fa988e4b9d4f1902e35466ee2d2af8
hugodob <hugo.dobbelaere@polytechnique.edu>
2017-07-06 15:47:41 -0700

    comments

--- classification.py
+++ classification.py
@@ -6,6 +6,7 @@ from keras.layers import Dense,Activation, Dropout
 from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 
+#Fits the data set in a training set (80% of the data) and a test set (20% of the data)
 def prepare_data_set(labels, list_labels, model, size_vectors):
     n_class=len(list_labels)
     n=len(labels)
@@ -17,6 +18,7 @@ def prepare_data_set(labels, list_labels, model, size_vectors):
     test_labels=np.zeros((n_test,n_class))
     for i in range(n_train):
         train_arrays[i]=model.docvecs[str(i)]
+        #Here the labels are converted to 9 elements vectors, each vector corresponding to a certain type
         train_labels[i][list_labels.index(labels[i])]=1
     for i in range(n_test):
         test_arrays[i]=model.docvecs[str(i+n_train)]
@@ -24,6 +26,7 @@ def prepare_data_set(labels, list_labels, model, size_vectors):
     return train_arrays, train_labels, test_arrays, test_labels
 
 
+#This function builds the MLP, trains it and saves it
 def train_class(size_vectors, train_arrays, train_labels, test_arrays, test_labels):
     mlp = Sequential()
     mlp.add(Dense(500,input_dim=size_vectors,activation='relu', kernel_initializer='lecun_uniform'))
